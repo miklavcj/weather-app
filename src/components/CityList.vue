ee346d01b25e97fa8f2d85a5a1c5a788cf029085
miklavcj <jaka.miklavc@gmail.com>
2021-01-03 22:12:31 +0100

    minor fixes

--- src/components/CityList.vue
+++ src/components/CityList.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container id="input-usage">
+  <v-container id="input-usage" class="mb-10 bx-auto" width="80%">
     <AddCity />
     <v-subheader>Saved Cities</v-subheader>
     <p v-if="cityList.length === 0">No cities</p>
@@ -11,35 +11,14 @@
         max-width="750"
         @click="selectedCity = city"
       >
-        <v-col cols="6">{{ city }}</v-col>
+        <v-col cols="6" offset="2">{{ city }}</v-col>
         <v-col offset="2">
-          <v-icon>mdi-white-balance-sunny</v-icon>
+          <!-- <v-icon>mdi-white-balance-sunny</v-icon> -->
           <v-icon @click="removeCity(index)">mdi-close</v-icon>
         </v-col>
       </v-row>
     </div>
   </v-container>
-  <!-- <div class="my-6" width="700">
-    <AddCity />
-    <p v-if="cityList.length === 0">No cities</p>
-    <v-card v-else class="mx-auto" max-width="750" tile>
-      <v-list>
-        <v-subheader>Saved Cities</v-subheader>
-        <v-list-item-group v-model="selectedItem" color="primary">
-          <v-list-item
-            v-for="(city, index) in cityList"
-            :key="index"
-            max-width="750"
-            @click="selectedCity = city"
-            >{{ city }}
-
-            <v-icon>mdi-white-balance-sunny</v-icon>
-            <v-icon @click="removeCity(index)">mdi-close</v-icon></v-list-item
-          >
-        </v-list-item-group>
-      </v-list>
-    </v-card>
-  </div> -->
 </template>
 
 <script>
